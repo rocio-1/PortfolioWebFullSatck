8ffa12682967208425214062e2a7b1a90d84b8bc
rocio vargas <93289629+rocio-1@users.noreply.github.com>
2022-08-03 02:21:35 -0300

    Agregue apache netbeans

---
+++ ProyectoFinal_RocioVargas/BACKEND/RocioVargasSalas/src/main/java/com/portfolio/RocioVargasSalas/Repository/IPersonaRepository.java
@@ -0,0 +1,12 @@
+
+package com.portfolio.RocioVargasSalas.Repository;
+
+import com.portfolio.RocioVargasSalas.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+    
+}
